1a731e9ed02ad08afe1e828989733870119da5a0
JashBerawala <learnjash@gmail..com>
2025-10-12 11:37:02 -0400

    Added my file(s)

---
+++ MyApplicationKotlinPractice/app/src/main/java/com/curious/myapplicationkotlinpractice/ui/practicekotlin.kt
@@ -0,0 +1,9 @@
+package com.curious.myapplicationkotlinpractice.ui
+
+fun main(){
+
+    println("Hello World!!!")
+
+    val x = 5
+    println(x)
+}
---
+++ MyApplicationKotlinPractice/gradle/practice.main.kts
@@ -0,0 +1,8 @@
+fun main(){
+
+    println("Hello World!!!")
+
+    val x = 5
+    println(x)
+
+
